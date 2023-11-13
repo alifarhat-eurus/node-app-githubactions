5adc6be1454270454e84f7028f80b0e6bc07d87a
alifarhat-eurus <ali.farhat@eurustechnologies.com>
2023-11-13 17:58:03 +0500

    Release:v4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello! Nouman, barrooooooooooooooooooooo :D ');
+    res.send('GCP, DevOps, Terraform, Azure, Jenkins :D ');
 });
 
 const port = 3000;
